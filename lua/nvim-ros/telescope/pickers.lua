3db847a6dde725eb33db55689b243c68196915ef
Sergey Alexandrov <alexandrov88@gmail.com>
2023-03-10 22:33:24 +0100

    feat(telescope): add `ed` picker for files in packages

--- lua/nvim-ros/telescope/pickers.lua
+++ lua/nvim-ros/telescope/pickers.lua
@@ -1,9 +1,13 @@
+local Logger = require('nvim-ros.logger')
 local ros = require('nvim-ros.ros')
 
-local pickers = require('telescope.pickers')
-local finders = require('telescope.finders')
-local previewers = require('telescope.previewers')
+local actions = require('telescope.actions')
+local actions_state = require('telescope.actions.state')
+local builtin = require('telescope.builtin')
 local conf = require('telescope.config').values
+local finders = require('telescope.finders')
+local pickers = require('telescope.pickers')
+local previewers = require('telescope.previewers')
 
 local M = {}
 
@@ -34,6 +38,42 @@ function M.msg_picker(opts)
           })
         end,
       }),
+      sorter = conf.generic_sorter(opts),
+    })
+    :find()
+end
+
+function M.ed_picker(opts)
+  opts = opts or {}
+  local packages = ros.list_packages()
+  pickers
+    .new(opts, {
+      prompt_title = 'ROS packages',
+      finder = finders.new_table({
+        results = packages,
+        entry_maker = function(entry)
+          return {
+            value = entry,
+            display = entry[1],
+            ordinal = entry[1],
+            path = entry[2],
+          }
+        end,
+      }),
+      attach_mappings = function(_, _)
+        actions.select_default:replace(function()
+          local selected_entry = actions_state.get_selected_entry()
+          if not selected_entry then
+            Logger:warn('No package entry selected')
+            return
+          end
+          builtin.find_files({
+            prompt_title = 'Find files in ROS package ' .. selected_entry.display,
+            cwd = selected_entry.path,
+          })
+        end)
+        return true
+      end,
       sorter = conf.generic_sorter(opts),
     })
     :find()
--- lua/telescope/_extensions/ros.lua
+++ lua/telescope/_extensions/ros.lua
@@ -3,5 +3,6 @@ local pickers = require('nvim-ros.telescope.pickers')
 return require('telescope').register_extension({
   exports = {
     msg = pickers.msg_picker,
+    ed = pickers.ed_picker,
   },
 })
