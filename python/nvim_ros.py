d5834f45bb732525889d91deb6824c72ea7b92de
Sergey Alexandrov <alexandrov88@gmail.com>
2024-02-26 14:40:08 +0000

    refactor(python): improve list_messages() implementation

--- python/nvim_ros.py
+++ python/nvim_ros.py
@@ -1,24 +1,31 @@
-def list_messages():
-    """
-    List all ROS messages in all packages.
+from pathlib import Path
+from typing import List, Tuple
+
+import rosmsg
+import rospkg
+
+
+def list_messages() -> List[Tuple[str, str]]:
+    """List all ROS messages in all packages.
 
     Returns a list of tuples (name, path) where name is the full name of the message in
     the form package_name/MessageName and path is the absolute path to the message file.
     """
-    import os
-    import rosmsg
-    import rospkg
     rospack = rospkg.RosPack()
     pkgs = rospack.list()
     packs = []
     for p in pkgs:
         package_paths = rosmsg._get_package_paths(p, rospack)
         for package_path in package_paths:
-            d = os.path.join(package_path, 'msg')
-            if os.path.isdir(d):
+            d = Path(package_path) / "msg"
+            if d.is_dir():
                 packs.append((p, d))
     msgs = []
-    for (p, direc) in packs:
-        for file in rosmsg._list_types(direc, 'msg', '.msg'):
-            msgs.append((f"{p}/{file}", os.path.join(direc, file) + '.msg'))
+    for p, d in packs:
+        msgs.extend(
+            [
+                (f"{p}/{file}", str((d / file).with_suffix(".msg")))
+                for file in rosmsg._list_types(d, "msg", ".msg")
+            ],
+        )
     return msgs
